1575f5b3a0b209a552278321188e15a63fc81559
Everton Paiva <evertonpaiva@gmail.com>
2019-06-26 16:09:49 -0300

    Melhorando tabulação das tags.

--- alurapic/src/App.vue
+++ alurapic/src/App.vue
@@ -3,7 +3,11 @@
   
     <nav>
       <ul>       
-        <li v-for="route in routes"><router-link :to="route.path ? route.path : '/'">{{ route.titulo }}</router-link></li>
+        <li v-for="route in routes">
+          <router-link :to="route.path ? route.path : '/'">
+            {{ route.titulo }}
+          </router-link>
+        </li>
       </ul>
     </nav>
 

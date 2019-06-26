4b41b9bf555ecbaa20ecd2962bf901da6f951006
Everton Paiva <evertonpaiva@gmail.com>
2019-06-26 16:13:35 -0300

    Realizando efeito de transição entre troca de páginas.

--- alurapic/src/App.vue
+++ alurapic/src/App.vue
@@ -11,7 +11,10 @@
       </ul>
     </nav>
 
-    <router-view></router-view>
+    <transition name="pagina">
+      <router-view></router-view>
+    </transition>
+    
   </div>  
 </template>
 
@@ -35,5 +38,12 @@ export default {
     font-family: Helvetica, sans-serif;
     width: 96%;
     margin: 0 auto;
+  }
+
+  .pagina-enter-active, .pagina-leave-active {
+    transition: opacity .3s
+  }
+  .pagina-enter, .pagina-leave-active {
+    opacity: 0
   }
 </style>

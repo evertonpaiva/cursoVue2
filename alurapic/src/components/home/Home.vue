bece1c0e71d63807a154f18dd3ab6fa314ab2204
Everton Paiva <evertonpaiva@gmail.com>
2019-06-27 10:34:06 -0300

    Lapidando componente de botao

--- alurapic/src/components/home/Home.vue
+++ alurapic/src/components/home/Home.vue
@@ -10,7 +10,7 @@
         <meu-painel :titulo="foto.titulo">
 
           <imagem-responsiva :url="foto.url" :titulo="foto.titulo"/>
-          <meu-botao tipo="button" rotulo="REMOVER" @botaoAtivado="remove(foto)" />
+          <meu-botao tipo="button" rotulo="REMOVER" @botaoAtivado="remove(foto)" :confirmacao="true" estilo="perigo" />
 
         </meu-painel>          
 

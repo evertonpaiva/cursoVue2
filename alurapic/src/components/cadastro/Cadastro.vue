fea9ba3c929990abf3d852ee0a7fd6a5a0246968
Everton Paiva <evertonpaiva@gmail.com>
2019-06-27 12:58:32 -0300

    Criando um modelo para representar as fotos.

--- alurapic/src/components/cadastro/Cadastro.vue
+++ alurapic/src/components/cadastro/Cadastro.vue
@@ -34,6 +34,7 @@
 
 import ImagemResponsiva from '../shared/imagem-responsiva/ImagemResponsiva.vue'
 import Botao from '../shared/botao/Botao.vue';
+import Foto from '../../domain/foto/Foto';
 
 export default {
 
@@ -46,11 +47,7 @@ export default {
   data() {
       return {
 
-          foto: {
-              titulo: '',
-              url: '',
-              descricao: ''
-          }
+          foto: new Foto()
       }
   },
 
@@ -60,11 +57,7 @@ export default {
 
           console.log(this.foto);
           console.log('enviar dados para a API');
-          this.foto = {
-              titulo: '',
-              url: '',
-              descricao: ''
-          }
+          this.foto = new Foto();          
       }
   }
 }

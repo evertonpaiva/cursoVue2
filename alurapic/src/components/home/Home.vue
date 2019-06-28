60938759144bab87aa9d8a1af8833cfa7604b1c3
Everton Paiva <evertonpaiva@gmail.com>
2019-06-28 11:18:53 -0300

    Tratamento de erros e notificação do usuário

--- alurapic/src/components/home/Home.vue
+++ alurapic/src/components/home/Home.vue
@@ -75,10 +75,7 @@ export default {
               let indice = this.fotos.indexOf(foto);
               this.fotos.splice(indice, 1);
               this.mensagem = 'Foto removida com sucesso';                
-            }, erro => {
-              console.log(err);
-            this.mensagem = 'Não foi possível remover a foto';
-            }
+            }, erro => this.mensagem = err.message
         );
     }
   },
@@ -89,7 +86,7 @@ export default {
     
     this.service
       .lista()
-      .then(fotos => this.fotos = fotos, err => console.log(err));
+      .then(fotos => this.fotos = fotos, err => this.mensagem = err.message);
   }
 }
 </script>
